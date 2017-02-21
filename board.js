66584d1cccbb7b77abea93bcd41f3832adfbb5d1
royhoo <roy_hoo@qq.com>
2017-02-21 13:17:11 +0800

    第2节代码

--- board.js
+++ board.js
@@ -5,16 +5,10 @@ var BOARD_HEIGHT = 577;
 var SQUARE_SIZE = 57;
 var SQUARE_LEFT = (BOARD_WIDTH - SQUARE_SIZE * 9) >> 1;
 var SQUARE_TOP = (BOARD_HEIGHT - SQUARE_SIZE * 10) >> 1;
-
 var PIECE_NAME = [
   "oo", null, null, null, null, null, null, null,
   "rk", "ra", "rb", "rn", "rr", "rc", "rp", null,
   "bk", "ba", "bb", "bn", "br", "bc", "bp", null,
-];
-var PIECE_NAME_CHN = [
-  "棋盘", null, null, null, null, null, null, null,
-  "红帥", "红仕", "红相", "红馬", "红車", "红砲", "红兵", null,
-  "黑将", "黑士", "黑象", "黑馬", "黑車", "黑炮", "黑卒", null,
 ];
 
 // 棋子距离棋盘左边框的距离
@@ -29,10 +23,12 @@ function SQ_Y(sq) {
 
 // Board对象的初始化代码，位于index.html中
 function Board(container, images) {
-  this.images = images;			// 图片路径
-  this.imgSquares = [];			// img数组，对应棋盘上的90个位置区域
+  this.images = images;		// 图片路径
+  this.imgSquares = [];		// img数组，对应棋盘上的90个位置区域
   this.pos = new Position();
   this.pos.fromFen("rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1");	// 根据FEN串初始化棋局
+  this.sqSelected = 0;		// 当前选中棋子的位置（如果为0，表示当前没有棋子被选中）
+  this.mvLast = 0;			// 上一步走法
 
   var style = container.style;
   style.position = "relative";
@@ -42,9 +38,9 @@ function Board(container, images) {
   var this_ = this;
   for (var sq = 0; sq < 256; sq ++) {
     // 遍历虚拟棋盘的256个点
-	
-	// 1.判断该点是否位于真实棋盘
-	if (!IN_BOARD(sq)) {
+  
+    // 1.判断该点是否位于真实棋盘
+    if (!IN_BOARD(sq)) {
       this.imgSquares.push(null);
       continue;
     }
@@ -58,7 +54,7 @@ function Board(container, images) {
     style.width = SQUARE_SIZE;
     style.height = SQUARE_SIZE;
     style.zIndex = 0;
-
+	
 	// 3.每个棋盘区域都会绑定点击事件，参数sq_表示了具体点击的区域。（这里用到了“闭包”的知识吧）
     img.onmousedown = function(sq_) {
       return function() {
@@ -77,15 +73,63 @@ function Board(container, images) {
   this.flushBoard();
 }
 
-// 点击棋盘的响应函数。点击棋盘（棋子或者空位置），就会调用该函数。sq_是点击的位置
-Board.prototype.clickSquare = function(sq_) {
-  alert("您点击了【"+PIECE_NAME_CHN[this.pos.squares[sq_]]+"】");
+// 判断这步棋是否合法，如果合法，则执行这步棋
+Board.prototype.addMove = function(mv) {
+  // 判断这步棋是否合法
+  if (!this.pos.legalMove(mv)) {
+    return;
+  }
+  
+  // 执行这步棋
+  if (!this.pos.makeMove(mv)) {
+    return;
+  }
+  
+  this.postAddMove(mv);
 }
 
-// 显示sq位置的棋子图片。如果该位置没棋子，则显示一张透明的图片
-Board.prototype.drawSquare = function(sq) {
+Board.prototype.postAddMove = function(mv) {
+  // 清除上一步的选中方框
+  if (this.mvLast > 0) {	
+    this.drawSquare(SRC(this.mvLast), false);
+    this.drawSquare(DST(this.mvLast), false);
+  }
+  
+  // 显示这一步走棋的选中方框
+  this.drawSquare(SRC(mv), true);
+  this.drawSquare(DST(mv), true);
+  
+  this.sqSelected = 0;
+  this.mvLast = mv;
+}
+
+// 点击棋盘的响应函数。无论是点击棋子还是空位置，都会调用该函数。sq_是点击的位置
+Board.prototype.clickSquare = function(sq_) {
+  var sq = sq_;						// 点击的位置
+  var pc = this.pos.squares[sq];	// 点击的棋子
+  if ((pc & SIDE_TAG(this.pos.sdPlayer)) != 0) {
+    // 点击了己方棋子，直接选中该子
+	
+	if (this.mvLast != 0) {
+      this.drawSquare(SRC(this.mvLast), false);
+      this.drawSquare(DST(this.mvLast), false);
+    }
+    if (this.sqSelected) {
+      this.drawSquare(this.sqSelected, false);
+    }
+    this.drawSquare(sq, true);
+    this.sqSelected = sq;
+  } else if (this.sqSelected > 0) {
+	// 点击的不是己方棋子（对方棋子或者无子的位置），但有子选中了(一定是自己的子)，那么执行这个走法
+    this.addMove(MOVE(this.sqSelected, sq));
+  }
+}
+
+// 显示sq位置的棋子图片。如果该位置没棋子，则显示一张透明的图片。如果selected为true，则要显示棋子选中时的边框。
+Board.prototype.drawSquare = function(sq, selected) {
   var img = this.imgSquares[sq];
   img.src = this.images + PIECE_NAME[this.pos.squares[sq]] + ".gif";
+  img.style.backgroundImage = selected ? "url(" + this.images + "oos.gif)" : "";
 }
 
 // 重新显示棋盘上的棋子
--- position.js
+++ position.js
@@ -15,6 +15,9 @@ var RANK_BOTTOM = 12;
 var FILE_LEFT = 3;
 var FILE_RIGHT = 11;
 
+var ADD_PIECE = false;	// 添加棋子
+var DEL_PIECE = true;	// 删除棋子
+
 // 辅助数组，用于判断棋子是否在棋盘上
 var IN_BOARD_ = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -35,9 +38,108 @@ var IN_BOARD_ = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ];
 
+// 辅助数组，用于判断将（帅）是否在九宫
+var IN_FORT_ = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+// 辅助数组，用于校验将（帅）、士（仕）、象（相）的走法是否合法
+var LEGAL_SPAN = [
+                       0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0,
+];
+
+// 辅助数组，用于校验马的走法是否合理。如果合理，返回对应马脚的方向；否则，返回0
+var KNIGHT_PIN_ = [
+                              0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,-16,  0,-16,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0, -1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0, -1,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0, 16,  0, 16,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,
+];
+
 // 判断某位置是否在棋盘
 function IN_BOARD(sq) {
   return IN_BOARD_[sq] != 0;
+}
+
+// 判断某位置是否在九宫
+function IN_FORT(sq) {
+  return IN_FORT_[sq] != 0;
 }
 
 // 根据一维矩阵，获取二维矩阵行数
@@ -84,21 +186,92 @@ function CHAR_TO_PIECE(c) {
   }
 }
 
+// 获得红黑标记(红子是8，黑子是16)
+function SIDE_TAG(sd) {
+  return 8 + (sd << 3);
+}
+
+// 获得对方红黑标记
+function OPP_SIDE_TAG(sd) {
+  return 16 - (sd << 3);
+}
+
+// 获取走法的起点
+function SRC(mv) {
+  return mv & 255;
+}
+
+// 获取走法的终点
+function DST(mv) {
+  return mv >> 8;
+}
+
+// 将一个走法的起点和终点，转化为一个整型数字
+function MOVE(sqSrc, sqDst) {
+  return sqSrc + (sqDst << 8);
+}
+
+// sp是棋子位置，sd是走棋方（红方0，黑方1）。返回兵（卒）向前走一步的位置。
+function SQUARE_FORWARD(sq, sd) {
+  return sq - 16 + (sd << 5);
+}
+
+// 校验将（帅）的走法
+function KING_SPAN(sqSrc, sqDst) {
+  return LEGAL_SPAN[sqDst - sqSrc + 256] == 1;
+}
+
+// 检验士（仕）的走法
+function ADVISOR_SPAN(sqSrc, sqDst) {
+  return LEGAL_SPAN[sqDst - sqSrc + 256] == 2;
+}
+
+// 校验象（相）的走法
+function BISHOP_SPAN(sqSrc, sqDst) {
+  return LEGAL_SPAN[sqDst - sqSrc + 256] == 3;
+}
+
+// 象眼的位置
+function BISHOP_PIN(sqSrc, sqDst) {
+  return (sqSrc + sqDst) >> 1;
+}
+
+// 如果马的走法合法，则返回相应马脚的位置。否则返回sqSrc。
+function KNIGHT_PIN(sqSrc, sqDst) {
+  return sqSrc + KNIGHT_PIN_[sqDst - sqSrc + 256];
+}
+
+// sp是棋子位置，sd是走棋方（红方0，黑方1）。如果该位置已过河，则返回true；否则返回false。
+function AWAY_HALF(sq, sd) {
+  return (sq & 0x80) == (sd << 7);
+}
+
+// 如果从起点sqSrc到终点sqDst没有过河，则返回true；否则返回false
+function SAME_HALF(sqSrc, sqDst) {
+  return ((sqSrc ^ sqDst) & 0x80) == 0;
+}
+
+// 如果sqSrc和sqDst在同一行则返回true；否则返回false
+function SAME_RANK(sqSrc, sqDst) {
+  return ((sqSrc ^ sqDst) & 0xf0) == 0;
+}
+
+// 如果sqSrc和sqDst在同一列则返回true；否则返回false
+function SAME_FILE(sqSrc, sqDst) {
+  return ((sqSrc ^ sqDst) & 0x0f) == 0;
+}
+
 function Position() {
   
 }
 
 // 初始化棋局数组
 Position.prototype.clearBoard = function() {
+  this.sdPlayer = 0;	// 该谁走棋。0-红方；1-黑方
   this.squares = [];	// 这个就是一维棋局数组
   for (var sq = 0; sq < 256; sq ++) {
     this.squares.push(0);
   }
-}
-
-// 将棋子pc添加进棋局中的sp位置
-Position.prototype.addPiece = function(sq, pc) {
-  this.squares[sq] = pc;
 }
 
 // 通过FEN串初始化棋局
@@ -148,4 +321,103 @@ Position.prototype.fromFen = function(fen) {
     return;
   }
 
+}
+
+// 判断步骤是否合法。是则返回true，否则返回false
+Position.prototype.legalMove = function(mv) {
+  var sqSrc = SRC(mv);						// 获取走法的起点位置
+  var pcSrc = this.squares[sqSrc];			// 获取起点位置的棋子
+  var pcSelfSide = SIDE_TAG(this.sdPlayer);	// 红黑标记(红子是8，黑子是16) 
+  
+  if ((pcSrc & pcSelfSide) == 0) {
+    // 起点位置的棋子，不是本方棋子。（是对方棋子，或者根本没有棋子）
+	return false;
+  }
+
+  var sqDst = DST(mv);				// 获取走法的终点位置
+  var pcDst = this.squares[sqDst];	// 获取终点位置的棋子
+  
+  if ((pcDst & pcSelfSide) != 0) {
+    // 终点位置有棋子，而且是本方棋子
+	return false;
+  }
+
+  switch (pcSrc - pcSelfSide) {
+  case PIECE_KING:		// 起点棋子是将（帅），校验走法
+    return IN_FORT(sqDst) && KING_SPAN(sqSrc, sqDst);
+  case PIECE_ADVISOR:	// 起点棋子是仕（仕），校验走法
+    return IN_FORT(sqDst) && ADVISOR_SPAN(sqSrc, sqDst);
+  case PIECE_BISHOP:	// 起点棋子是象（相），校验走法
+    return SAME_HALF(sqSrc, sqDst) && BISHOP_SPAN(sqSrc, sqDst) &&
+        this.squares[BISHOP_PIN(sqSrc, sqDst)] == 0;
+  case PIECE_KNIGHT:	// 起点棋子是马，校验走法
+    var sqPin = KNIGHT_PIN(sqSrc, sqDst);
+    return sqPin != sqSrc && this.squares[sqPin] == 0;
+  case PIECE_ROOK:		// 起点棋子是车，校验走法
+  case PIECE_CANNON:	// 起点棋子是炮，校验走法
+    var delta;			// 标识沿哪个方向走棋
+    if (SAME_RANK(sqSrc, sqDst)) {
+	  // 起点和终点位于同一行。再根据起点和终点的大小关系，判断具体是沿哪个方向走棋。
+      delta = (sqDst < sqSrc ? -1 : 1);
+    } else if (SAME_FILE(sqSrc, sqDst)) {
+	  // 起点和终点位于同一列。再根据起点和终点的大小关系，判断具体是沿哪个方向走棋。
+      delta = (sqDst < sqSrc ? -16 : 16);
+    } else {
+	  // 起点和终点不在同一行，也不在同一列。走法是非法的。
+      return false;
+    }
+    var sqPin = sqSrc + delta;	// 沿着方向delta走一步棋
+    while (sqPin != sqDst && this.squares[sqPin] == 0) {
+      // 沿方向delta一步步向前走，直到遇到棋子，或者sqPin走到了终点的位置上
+	  sqPin += delta;
+    }
+    if (sqPin == sqDst) {
+	  // 如果终点没有棋子，不管是车还是炮，这步棋都是合法的。如果是车，不管终点有没有棋子（对方棋子），这步棋都合法。
+      return pcDst == 0 || pcSrc - pcSelfSide == PIECE_ROOK;
+    }
+	// 此时已经翻山，终点必须有棋子，并且行棋的是炮，否则这步棋不合法
+    if (pcDst == 0 || pcSrc - pcSelfSide != PIECE_CANNON) {
+      return false;
+    }
+    sqPin += delta;
+    while (sqPin != sqDst && this.squares[sqPin] == 0) {
+      sqPin += delta;
+    }
+    return sqPin == sqDst;
+  case PIECE_PAWN:
+    // 兵已过河，并且是左右两个方向走的
+    if (AWAY_HALF(sqDst, this.sdPlayer) && (sqDst == sqSrc - 1 || sqDst == sqSrc + 1)) {
+      return true;
+    }
+	// 判断兵是不是在向前走
+    return sqDst == SQUARE_FORWARD(sqSrc, this.sdPlayer);
+  default:
+    return false;
+  }
+}
+
+// 走一步棋
+Position.prototype.makeMove = function(mv) {
+  this.movePiece(mv);
+  return true;
+}
+
+// 根据走法移动棋子，删除终点位置的棋子，并将起点位置的棋子放置在终点的位置。
+Position.prototype.movePiece = function(mv) {
+  var sqSrc = SRC(mv);			// 起点位置
+  var sqDst = DST(mv);			// 终点位置
+  var pc = this.squares[sqDst];	// 终点位置的棋子
+  if (pc > 0) {
+    // 终点有棋子，要删除该棋子
+    this.addPiece(sqDst, pc, DEL_PIECE);
+  }
+  pc = this.squares[sqSrc];				// 起点位置的棋子
+  this.addPiece(sqSrc, pc, DEL_PIECE);	// 删除起点棋子
+  this.addPiece(sqDst, pc, ADD_PIECE);	// 将原来起点的棋子添加到终点
+}
+
+// 如果bDel为false，则将棋子pc添加进棋局中的sp位置；如果bDel为true，则删除sp位置的棋子。
+Position.prototype.addPiece = function(sq, pc, bDel) {
+  var pcAdjust;
+  this.squares[sq] = bDel ? 0 : pc;
 }
